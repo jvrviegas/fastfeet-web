d9832aedde03da922fb712efb8e702836405cf7a
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-27 15:28:49 -0300

    D - Sign In - Add form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,32 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/fastfeet-logo.png';
+
+import { Container, Content } from './styles';
 
 export default function SignIn() {
-  return <h1>Sign In</h1>;
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="FastFeet" />
+
+        <Form>
+          <label htmlFor="email">Seu e-mail</label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="exemplo@email.com"
+          />
+
+          <label htmlFor="email">Sua senha</label>
+          <Input name="password" type="password" placeholder="************" />
+
+          <button type="submit">Entrar no sistema</button>
+        </Form>
+      </Content>
+    </Container>
+  );
 }
