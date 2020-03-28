48629bbd0ae541dbd1407844f49c10f86bcff77c
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-28 12:16:10 -0300

    D - Routes - Add recipients and deliveries problems routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ import Route from './Route';
 import SignIn from '~/pages/SignIn';
 import Dashboard from '~/pages/Dashboard';
 import Deliverymans from '~/pages/Deliverymans';
+import Recipients from '~/pages/Recipients';
+import DeliveriesProblems from '~/pages/DeliveriesProblems';
 
 export default function Routes() {
   return (
@@ -12,6 +14,12 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/dashboard" isPrivate component={Dashboard} />
       <Route path="/deliverymans" isPrivate component={Deliverymans} />
+      <Route path="/recipients" isPrivate component={Recipients} />
+      <Route
+        path="/deliveries-problems"
+        isPrivate
+        component={DeliveriesProblems}
+      />
     </Switch>
   );
 }
