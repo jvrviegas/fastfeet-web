8b21a3df8205198208425b9a2defd5efb2603719
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-28 11:39:10 -0300

    M - Routes - Import new route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
 import Dashboard from '~/pages/Dashboard';
+import Deliverymans from '~/pages/Deliverymans';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/dashboard" isPrivate component={Dashboard} />
+      <Route path="/deliverymans" isPrivate component={Deliverymans} />
     </Switch>
   );
 }
