6f5e67d49265836acc0065c1e9614e4e92740326
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-28 11:47:27 -0300

    M - Sign In Page - Remove container and content elements

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -4,29 +4,25 @@ import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/fastfeet-logo.png';
 
-import { Container, Content } from './styles';
-
 export default function SignIn() {
   return (
-    <Container>
-      <Content>
-        <img src={logo} alt="FastFeet" />
+    <>
+      <img src={logo} alt="FastFeet" />
 
-        <Form>
-          <label htmlFor="email">Seu e-mail</label>
-          <Input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="exemplo@email.com"
-          />
+      <Form>
+        <label htmlFor="email">Seu e-mail</label>
+        <Input
+          id="email"
+          name="email"
+          type="email"
+          placeholder="exemplo@email.com"
+        />
 
-          <label htmlFor="email">Sua senha</label>
-          <Input name="password" type="password" placeholder="************" />
+        <label htmlFor="email">Sua senha</label>
+        <Input name="password" type="password" placeholder="************" />
 
-          <button type="submit">Entrar no sistema</button>
-        </Form>
-      </Content>
-    </Container>
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
   );
 }
