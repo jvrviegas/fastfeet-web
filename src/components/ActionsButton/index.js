8c0d58e5a324a8e84244d16a712754e4dfd2e84c
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-29 16:30:00 -0300

    D - Components - Add actions button

---
+++ src/components/ActionsButton/index.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-nested-ternary */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  MdMoreHoriz,
+  MdVisibility,
+  MdCreate,
+  MdDeleteForever,
+} from 'react-icons/md';
+
+import { Container, ActionsList, MoreActions, Action } from './styles';
+
+export default function ActionsButton({ actions }) {
+  const [visible, setVisible] = useState(false);
+
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
+
+  return (
+    <Container>
+      <MoreActions onClick={handleToggleVisible}>
+        <MdMoreHoriz color="#C6C6C6" size={20} />
+      </MoreActions>
+
+      <ActionsList visible={visible}>
+        <div>
+          {actions.map((action) =>
+            action === 'Visualizar' ? (
+              <Action>
+                <MdVisibility size={16} color="#8E5BE8" />
+                <span>{action}</span>
+              </Action>
+            ) : action === 'Editar' ? (
+              <Action>
+                <MdCreate size={16} color="#4D85EE" />
+                <span>{action}</span>
+              </Action>
+            ) : (
+              <Action>
+                <MdDeleteForever size={16} color="#DE3B3B" />
+                <span>{action}</span>
+              </Action>
+            )
+          )}
+        </div>
+      </ActionsList>
+    </Container>
+  );
+}
+
+ActionsButton.propTypes = {
+  actions: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
---
+++ src/components/ActionsButton/styles.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+`;
+
+export const MoreActions = styled.button`
+  background: none;
+  border: 0;
+  position: relative;
+`;
+
+export const ActionsList = styled.aside`
+  position: absolute;
+  width: 140px;
+  left: calc(50% - 65px);
+  top: calc(100% + 8px);
+  background: #fff;
+  border-radius: 4px;
+  padding: 5px 10px;
+  display: ${(props) => (props.visible ? 'flex' : 'none')};
+  border: 1px solid #eee;
+  z-index: 1;
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: calc(50% - 8px);
+    top: -8px;
+    width: 0;
+    height: 0;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-bottom: 8px solid #eee;
+  }
+
+  div {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const Action = styled.button`
+  width: 100%;
+  display: flex;
+  border: 0;
+  background: transparent;
+  text-align: left;
+  font-size: 16px;
+  color: #999;
+  padding: 10px 0;
+
+  svg {
+    margin-right: 5px;
+  }
+
+  & + button {
+    border-top: 1px solid #eee;
+  }
+`;
