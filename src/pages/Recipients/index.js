23e5a127aa05f7ccf7caac7362a25c1e156586ba
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-29 16:31:20 -0300

    D - Recipients Page - Add content

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,7 +1,60 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import ContentHeader from '~/components/ContentHeader';
+import ActionsButton from '~/components/ActionsButton';
+
+const actions = ['Editar', 'Excluir'];
 
 export default function Recipients() {
-  return <h1>Recipients</h1>;
+  return (
+    <>
+      <h2>Gerenciando destinatários</h2>
+      <ContentHeader title="destinatários" />
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Endereço</th>
+            <th style={{ textAlign: 'center' }}>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#01</td>
+            <td>John Doe</td>
+            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>John Doe</td>
+            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>John Doe</td>
+            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>John Doe</td>
+            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
