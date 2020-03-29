967bb6b96bff7b6f3c7ef748b61e3ca916b78354
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-29 17:13:26 -0300

    D - Deliveries Problems Page - Add content

--- src/pages/DeliveriesProblems/index.js
+++ src/pages/DeliveriesProblems/index.js
@@ -1,7 +1,56 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import ActionsButton from '~/components/ActionsButton';
+
+const actions = ['Visualizar', 'Excluir encomenda'];
 
 export default function DeliveriesProblems() {
-  return <h1>Deliveries Problems</h1>;
+  return (
+    <>
+      <h2>Problemas na entrega</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Encomenda</th>
+            <th>Problema</th>
+            <th style={{ textAlign: 'center' }}>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#01</td>
+            <td>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in
+              mauris et felis eleifend elementum vel quis lectus…
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} larger />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>Endereço não encontrado</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} larger />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>Destinatário ausente</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} larger />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>Infelizmente sofri um acidente que danificou a encomenda</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} larger />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
