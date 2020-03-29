f48cc87da89af3158f8460aa698b851a61e0179a
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-29 16:31:06 -0300

    D - Deliverymans Page - Add content

--- src/pages/Deliverymans/index.js
+++ src/pages/Deliverymans/index.js
@@ -1,7 +1,85 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import ActionsButton from '~/components/ActionsButton';
+import ContentHeader from '~/components/ContentHeader';
+
+const actions = ['Editar', 'Excluir'];
 
 export default function Deliverymans() {
-  return <h1>Deliverymans</h1>;
+  return (
+    <>
+      <h2>Gerenciando entregadores</h2>
+      <ContentHeader title="entregadores" />
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Foto</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th style={{ textAlign: 'center' }}>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#01</td>
+            <td>
+              <img
+                src="https://ui-avatars.com/api/?name=John+Doe&background=F4EFFC&color=A28FD0"
+                alt=""
+              />
+            </td>
+            <td>John Doe</td>
+            <td>example@rocketseat.com</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>
+              <img
+                src="https://ui-avatars.com/api/?name=John+Doe&background=F4EFFC&color=A28FD0"
+                alt=""
+              />
+            </td>
+            <td>John Doe</td>
+            <td>example@rocketseat.com</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>
+              <img
+                src="https://ui-avatars.com/api/?name=John+Doe&background=F4EFFC&color=A28FD0"
+                alt=""
+              />
+            </td>
+            <td>John Doe</td>
+            <td>example@rocketseat.com</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+          <tr>
+            <td>#01</td>
+            <td>
+              <img
+                src="https://ui-avatars.com/api/?name=John+Doe&background=F4EFFC&color=A28FD0"
+                alt=""
+              />
+            </td>
+            <td>John Doe</td>
+            <td>example@rocketseat.com</td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
