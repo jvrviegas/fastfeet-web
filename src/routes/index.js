3fc11be06aa24bb11b2edfdd9704f812c69274d3
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-29 22:45:51 -0300

    M - Routes index - Change dashboard to List Orders

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignIn from '~/pages/SignIn';
-import Dashboard from '~/pages/Dashboard';
+import ListOrders from '~/pages/Orders/ListOrders';
+import CreateOrder from '~/pages/Orders/CreateOrder';
 import Deliverymans from '~/pages/Deliverymans';
 import Recipients from '~/pages/Recipients';
 import DeliveriesProblems from '~/pages/DeliveriesProblems';
@@ -12,11 +13,20 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" isPrivate component={Dashboard} />
-      <Route path="/deliverymans" isPrivate component={Deliverymans} />
-      <Route path="/recipients" isPrivate component={Recipients} />
+      <Route path="/dashboard/orders" exact isPrivate component={ListOrders} />
       <Route
-        path="/deliveries-problems"
+        path="/dashboard/orders/create"
+        isPrivate
+        component={CreateOrder}
+      />
+      <Route
+        path="/dashboard/deliverymans"
+        isPrivate
+        component={Deliverymans}
+      />
+      <Route path="/dashboard/recipients" isPrivate component={Recipients} />
+      <Route
+        path="/dashboard/deliveries-problems"
         isPrivate
         component={DeliveriesProblems}
       />
