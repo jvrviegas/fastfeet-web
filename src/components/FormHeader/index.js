65a8c76392b3fc0d15a3e28b8e6a93bdf94f39ad
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-01 14:42:45 -0300

    D - Form Header Component - Add history and button redirect

--- src/components/FormHeader/index.js
+++ src/components/FormHeader/index.js
@@ -5,12 +5,18 @@ import { MdKeyboardArrowLeft, MdCheck } from 'react-icons/md';
 
 import { Container } from './styles';
 
+import history from '~/services/history';
+
 export default function FormHeader({ title = '' }) {
+  function handleReturn() {
+    history.push('/dashboard/orders');
+  }
+
   return (
     <Container>
       <h2>{title}</h2>
       <div>
-        <button id="return" type="button">
+        <button id="return" type="button" onClick={handleReturn}>
           <MdKeyboardArrowLeft size={24} />
           Voltar
         </button>
