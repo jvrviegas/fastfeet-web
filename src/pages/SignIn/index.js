e3fa640da3f3a812c9aecb1eb4080128c1ae50d6
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-01 18:11:24 -0300

    D - Sign In Page - Add auth and schema validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,15 +1,33 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/fastfeet-logo.png';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <Form>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <label htmlFor="email">Seu e-mail</label>
         <Input
           id="email"
@@ -21,7 +39,9 @@ export default function SignIn() {
         <label htmlFor="email">Sua senha</label>
         <Input name="password" type="password" placeholder="************" />
 
-        <button type="submit">Entrar no sistema</button>
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Entrar no sistema'}
+        </button>
       </Form>
     </>
   );
