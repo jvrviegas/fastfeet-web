ad5ee62bd11bbe2807cbaaca741549d4d51a015b
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-03 10:52:01 -0300

    D - Auth Sagas - Add set token and sign out

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -22,6 +22,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard/orders');
@@ -31,4 +33,22 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
