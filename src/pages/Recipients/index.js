14933a8f9b7916a2c4ab9b5a178c0e0b60326d89
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-07 16:33:26 -0300

    D - Recipients Page - Load recipients dynamically

--- src/pages/Recipients/index.js
+++ src/pages/Recipients/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaSpinner } from 'react-icons/fa';
+
+import api from '~/services/api';
 
 import ContentHeader from '~/components/ContentHeader';
 import ActionsButton from '~/components/ActionsButton';
@@ -6,55 +9,72 @@ import ActionsButton from '~/components/ActionsButton';
 const actions = ['Editar', 'Excluir'];
 
 export default function Recipients() {
+  const [recipients, setRecipients] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadRecipients() {
+      setLoading(true);
+
+      const response = await api.get(`/recipients`, {
+        params: {
+          name: '',
+        },
+      });
+
+      setRecipients(response.data);
+      setLoading(false);
+    }
+
+    loadRecipients();
+  }, []);
+
+  function renderTableData() {
+    if (recipients.length > 0) {
+      return recipients.map((recipient) => {
+        return (
+          <tr>
+            <td>#{recipient.id}</td>
+            <td>{recipient.name}</td>
+            <td>
+              {`${recipient.street_name}, ${recipient.number}, ${recipient.town} - ${recipient.state}`}
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <ActionsButton actions={actions} />
+            </td>
+          </tr>
+        );
+      });
+    }
+
+    return (
+      <div>
+        <span>Você não possui nenhum destinatário cadastrado</span>
+      </div>
+    );
+  }
   return (
     <>
       <h2>Gerenciando destinatários</h2>
-      <ContentHeader title="destinatários" />
+      <ContentHeader title="destinatários" page="recipients" />
 
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Endereço</th>
-            <th style={{ textAlign: 'center' }}>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>#01</td>
-            <td>John Doe</td>
-            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} />
-            </td>
-          </tr>
-          <tr>
-            <td>#01</td>
-            <td>John Doe</td>
-            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} />
-            </td>
-          </tr>
-          <tr>
-            <td>#01</td>
-            <td>John Doe</td>
-            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} />
-            </td>
-          </tr>
-          <tr>
-            <td>#01</td>
-            <td>John Doe</td>
-            <td>Rua Beethoven, 1729, Diadema - São Paulo</td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {loading ? (
+        <div className="loading">
+          <FaSpinner size={30} />
+        </div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Endereço</th>
+              <th style={{ textAlign: 'center' }}>Ações</th>
+            </tr>
+          </thead>
+          <tbody>{renderTableData()}</tbody>
+        </table>
+      )}
     </>
   );
 }
