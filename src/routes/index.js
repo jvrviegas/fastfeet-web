3a85044f9406824f4fa4369ee674512728576539
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-07 17:42:37 -0300

    D - Routes - Add deliverymans form

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import SignIn from '~/pages/SignIn';
 import Orders from '~/pages/Orders';
 import OrdersForm from '~/pages/OrdersForm';
 import Deliverymans from '~/pages/Deliverymans';
+import DeliverymansForm from '~/pages/DeliverymansForm';
 import Recipients from '~/pages/Recipients';
 import DeliveriesProblems from '~/pages/DeliveriesProblems';
 import Profile from '~/pages/Profile';
@@ -20,7 +21,13 @@ export default function Routes() {
       <Route path="/orders/create" isPrivate component={OrdersForm} />
       {/* <Route path="/orders/update" isPrivate component={OrdersForm} /> */}
 
-      <Route path="/deliverymans" isPrivate component={Deliverymans} />
+      <Route path="/deliverymans" exact isPrivate component={Deliverymans} />
+      <Route
+        path="/deliverymans/create"
+        isPrivate
+        component={DeliverymansForm}
+      />
+
       <Route path="/recipients" isPrivate component={Recipients} />
       <Route
         path="/deliveries-problems"
