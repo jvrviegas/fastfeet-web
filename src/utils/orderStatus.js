ff7889452870dcea07587dfa95d6c4f0e3575542
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-08 22:22:13 -0300

    M - Order Status Utils - Fix canceled order name

--- src/utils/orderStatus.js
+++ src/utils/orderStatus.js
@@ -1,26 +1,26 @@
 export default function orderStatus(order) {
-  if (!order.cancelled_at && order.start_date && order.end_date) {
+  if (!order.canceled_at && order.start_date && order.end_date) {
     return {
       id: 1,
       name: 'ENTREGUE',
     };
   }
 
-  if (!order.cancelled_at && !order.start_date && !order.end_date) {
+  if (!order.canceled_at && !order.start_date && !order.end_date) {
     return {
       id: 2,
       name: 'PENDENTE',
     };
   }
 
-  if (!order.cancelled_at && order.start_date && !order.end_date) {
+  if (!order.canceled_at && order.start_date && !order.end_date) {
     return {
       id: 3,
       name: 'RETIRADA',
     };
   }
 
-  if (order.cancelled_at) {
+  if (order.canceled_at) {
     return {
       id: 4,
       name: 'CANCELADA',
