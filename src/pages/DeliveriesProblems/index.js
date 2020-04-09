42606254006329b9b341772d3c20324847181947
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-09 13:55:15 -0300

    M - Deliveries Problems Page - Remove render table data function and add to conent

--- src/pages/DeliveriesProblems/index.js
+++ src/pages/DeliveriesProblems/index.js
@@ -1,56 +1,82 @@
-import React from 'react';
+/* eslint-disable no-nested-ternary */
+import React, { useState, useEffect } from 'react';
+import { FaSpinner } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
 
 import ActionsButton from '~/components/ActionsButton';
 
 const actions = ['Visualizar', 'Cancelar encomenda'];
 
 export default function DeliveriesProblems() {
+  const [deliveriesProblems, setDeliveriesProblems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const loadDeliveriesProblems = async () => {
+    setLoading(true);
+
+    const response = await api.get(`/deliveries/problems`);
+
+    setDeliveriesProblems(response.data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    loadDeliveriesProblems();
+  }, []);
+
+  async function handleCancelDelivery(id) {
+    await api.put(`/problem/${id}/cancel-delivery`);
+    toast.success('Encomenda cancelada com sucesso!');
+    loadDeliveriesProblems();
+  }
+
   return (
     <>
       <h2>Problemas na entrega</h2>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Encomenda</th>
-            <th>Problema</th>
-            <th style={{ textAlign: 'center' }}>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>#01</td>
-            <td>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in
-              mauris et felis eleifend elementum vel quis lectus…
-            </td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} larger />
-            </td>
-          </tr>
-          <tr>
-            <td>#01</td>
-            <td>Endereço não encontrado</td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} larger />
-            </td>
-          </tr>
-          <tr>
-            <td>#01</td>
-            <td>Destinatário ausente</td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} larger />
-            </td>
-          </tr>
-          <tr>
-            <td>#01</td>
-            <td>Infelizmente sofri um acidente que danificou a encomenda</td>
-            <td style={{ textAlign: 'center' }}>
-              <ActionsButton actions={actions} larger />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {loading ? (
+        <div className="loading">
+          <FaSpinner size={30} />
+        </div>
+      ) : deliveriesProblems.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Encomenda</th>
+              <th>Problema</th>
+              <th style={{ textAlign: 'center' }}>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {deliveriesProblems.map((deliveryProblem) => (
+              <tr>
+                <td>
+                  #
+                  {deliveryProblem.id < 10
+                    ? `0${deliveryProblem.id}`
+                    : deliveryProblem.id}
+                </td>
+                <td>{deliveryProblem.description}</td>
+                <td style={{ textAlign: 'center' }}>
+                  <ActionsButton
+                    actions={actions}
+                    larger
+                    page="deliveries-problems"
+                    data={deliveryProblem}
+                    callback={() => handleCancelDelivery(deliveryProblem.id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="empty-list">
+          <span>Você não possui encomendas com problemas</span>
+        </div>
+      )}
     </>
   );
 }
