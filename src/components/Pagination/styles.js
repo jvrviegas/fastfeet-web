ee0150bd103149ea0015dfbdd8bea00d4d3c185f
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-10 17:17:18 -0300

    M - Pagination Styles - Add box shadow

--- src/components/Pagination/styles.js
+++ src/components/Pagination/styles.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: fixed;
   width: 80%;
   min-width: 600px;
-  position: fixed;
   bottom: 0;
   margin: 0 auto;
   background: #fff;
   height: 45px;
+  -webkit-box-shadow: 0px -1px 5px 0px rgba(0, 0, 0, 0.3);
+  -moz-box-shadow: 0px -1px 5px 0px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px -1px 5px 0px rgba(0, 0, 0, 0.3);
 
   justify-content: space-between;
   align-items: center;
   align-content: center;
 
   button {
-    background: #ccc;
     height: 100%;
     margin-left: 5px;
     margin-right: 5px;
