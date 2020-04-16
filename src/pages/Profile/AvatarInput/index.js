b2f25fedbcd0e98afe64d270ed549032ccba4925
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-16 15:50:17 -0300

    M - Avatar Input - Remove console.tron.log

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -24,7 +24,7 @@ export default function AvatarInput() {
     const data = new FormData();
 
     data.append('file', e.target.files[0]);
-
+    console.tron.log(e.target.files[0]);
     const response = await api.post('files', data);
 
     const { id, url } = response.data;
